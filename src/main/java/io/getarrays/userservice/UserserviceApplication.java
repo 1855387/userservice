96470127fe457abb8706fb501389cf67079a1f77
Charlie <trollol678534@gmail.com>
2022-04-25 16:52:07 -0700

    push today's work

--- src/main/java/io/getarrays/userservice/UserserviceApplication.java
+++ src/main/java/io/getarrays/userservice/UserserviceApplication.java
@@ -1,7 +1,14 @@
 package io.getarrays.userservice;
 
+import io.getarrays.userservice.domain.Role;
+import io.getarrays.userservice.domain.User;
+import io.getarrays.userservice.service.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class UserserviceApplication {
@@ -9,6 +16,31 @@ public class UserserviceApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(UserserviceApplication.class, args);
+	}
+
+
+	@Bean
+	CommandLineRunner run(UserService userService) { // testing the database with role adds etc
+		return args -> {
+			userService.saveRole(new Role(null, "ROLE_USER"));
+			userService.saveRole(new Role(null, "ROLE_MANAGER"));
+			userService.saveRole(new Role(null, "ROLE_ADMIN"));
+			userService.saveRole(new Role(null, "ROLE_SUPER_ADMIN"));
+
+			userService.saveUser((new User(null, "John Travolta", "john", "1234", new ArrayList<>())));
+			userService.saveUser((new User(null, "Will Smith", "will", "1234", new ArrayList<>())));
+			userService.saveUser((new User(null, "HAP", "ha", "1234", new ArrayList<>())));
+			userService.saveUser((new User(null, "yet", "y", "y", new ArrayList<>())));
+
+			userService.addRoletoUser("john", "ROLE_USER");
+			userService.addRoletoUser("john", "ROLE_MANAGER");
+			userService.addRoletoUser("will", "ROLE_MANAGER");
+			userService.addRoletoUser("ha", "ROLE_ADMIN");
+			userService.addRoletoUser("y", "ROLE_ADMIN");
+			userService.addRoletoUser("y", "ROLE_SUPER_ADMIN");
+			userService.addRoletoUser("y", "ROLE_USER");
+
+		};
 	}
 
 }
--- src/main/java/io/getarrays/userservice/api/UserResource.java
+++ src/main/java/io/getarrays/userservice/api/UserResource.java
@@ -1,13 +1,15 @@
 package io.getarrays.userservice.api;
 
+import io.getarrays.userservice.domain.Role;
 import io.getarrays.userservice.domain.User;
 import io.getarrays.userservice.service.UserService;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -17,7 +19,32 @@ public class UserResource {
     private final UserService userService;
 
     @GetMapping("/users")
-    public ResponseEntity<List<User>>getUsers(){
+    public ResponseEntity<List<User>> getUsers() {
         return ResponseEntity.ok().body(userService.getUsers()); //returns list of all the users
     }
+
+    @PostMapping("/user/save") // is post mapping because you want to send a post when saving a new user
+    public ResponseEntity<User> saveUsers(@RequestBody User user) { //passing in user to save
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/user/save").toUriString());
+        return ResponseEntity.created(uri).body(userService.saveUser(user));
+    }
+
+    @PostMapping("/role/save") // is post mapping because you want to send a post when saving a new user
+    public ResponseEntity<Role> saveRole(@RequestBody Role role) { //passing in user to save
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/role/save").toUriString());
+        return ResponseEntity.created(uri).body(userService.saveRole(role));
+    }
+
+    @PostMapping("/role/addtouser") // is post mapping because you want to send a post when saving a new user
+    public ResponseEntity<?> addRoletoUser(@RequestBody RoleToUserForm form) { //passing in user to save
+        userService.addRoletoUser(form.getUsername(), form.getRoleName());
+        return ResponseEntity.ok().build();
+    }
+
+}
+
+@Data
+class RoleToUserForm {
+    private String username;
+    private String roleName;
 }
